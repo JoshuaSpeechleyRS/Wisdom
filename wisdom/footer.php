6ddf1450666ff7d5bf76f1fc09f277572c58f3b8
Ed Sloane <ed@residencystudios.com>
2021-06-10 10:50:26 +1000

    Footer links.

--- wisdom/footer.php
+++ wisdom/footer.php
@@ -3,14 +3,14 @@
   <div class="clearfix">
   <div class="container top-row">
 	  <div class="row">
-	  	<img src="<?php bloginfo('template_directory'); ?>/img/wisdom-logo.png" alt="" />
+	  	<a href="/"><img src="<?php bloginfo('template_directory'); ?>/img/wisdom-logo.png" alt="" /></a>
 	  	<ul>
 		  	<li><a href="<?php bloginfo('url'); ?>/course-list/">Search Courses</a></li>
 		  	<li><a href="<?php bloginfo('url'); ?>/workshop-list/">Search Workshops</a></li>
 		  	<li class="social-buttons">
-		  		<img src="<?php bloginfo('template_directory'); ?>/img/icon-facebook.png" alt="" />
-		  		<img src="<?php bloginfo('template_directory'); ?>/img/icon-linkedin.png" alt="" />
-		  		<img src="<?php bloginfo('template_directory'); ?>/img/icon-instagram.png" alt="" />
+		  		<a href="https://www.facebook.com/WisdomLearning/"><img src="<?php bloginfo('template_directory'); ?>/img/icon-facebook.png" alt="" /></a>
+		  		<a href="https://www.linkedin.com/company/wisdom-learning/"><img src="<?php bloginfo('template_directory'); ?>/img/icon-linkedin.png" alt="" /></a>
+		  		<a href="https://www.instagram.com/wisdomlearning/"><img src="<?php bloginfo('template_directory'); ?>/img/icon-instagram.png" alt="" /></a>
 		  	</li>
 	  	</ul>
 	  </div>
